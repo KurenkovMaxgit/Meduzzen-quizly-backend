ef26067655e0d9826bd9e319b83d51519c8bd830
Maksym Kurenkov <kurenkov.maxim3000@gmail.com>
2026-01-29 12:56:49 +0200

    feat: add global configuration file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuraion';
+import { envValidationSchema } from './config/env.schema';
+
+const configModule = ConfigModule.forRoot({
+  load: [configuration],
+  isGlobal: true,
+  validationSchema: envValidationSchema,
+});
 
 @Module({
-  imports: [],
+  imports: [configModule],
   controllers: [AppController],
   providers: [AppService],
 })
