3b7e5e7de0945d9d2d56edf774730c3165b2e536
Maksym Kurenkov <kurenkov.maxim3000@gmail.com>
2026-01-31 03:59:37 +0200

    feat: add database connection with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuraion';
 import { envValidationSchema } from './config/env.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 const configModule = ConfigModule.forRoot({
   load: [configuration],
@@ -12,7 +15,23 @@ const configModule = ConfigModule.forRoot({
 });
 
 @Module({
-  imports: [configModule],
+  imports: [
+    configModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
