de1a841bb5ac6e3d8c3749d96e940a38fd6f8cbc
Maksym Kurenkov <kurenkov.maxim3000@gmail.com>
2026-02-01 23:48:00 +0200

    refactor: correct typos in .env.example & config file import. Fix import consistency in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,32 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import configuration from './config/configuraion';
+import configuration from './config/configuration';
 import { envValidationSchema } from './config/env.schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { User } from './user/user.entity';
-
-const configModule = ConfigModule.forRoot({
-  load: [configuration],
-  isGlobal: true,
-  validationSchema: envValidationSchema,
-});
 
 @Module({
   imports: [
-    configModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('POSTGRES_HOST'),
-        port: configService.get<number>('POSTGRES_PORT'),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DB'),
-        entities: [User],
-        synchronize: true,
+        host: configService.get<string>('dbHost'),
+        port: configService.get<number>('dbPort'),
+        username: configService.get<string>('dbUser'),
+        password: configService.get<string>('dbPassword'),
+        database: configService.get<string>('dbName'),
+        entities: ['common/entities/*.ts'],
+        synchronize: configService.get<string>('nodeEnv') !== 'production',
+        migrations: [__dirname + '../migrations/**/*{.js,.ts}'],
+        migrationsTableName: 'migrations_table',
       }),
     }),
     UserModule,
--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -2,6 +2,11 @@ import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
   PORT: Joi.number().default(3000),
-  NODE_MODE: Joi.string().required(), 
-  CLIENT_URL: Joi.string().required() //TODO: add .uri() validation when setting correct client uri
+  NODE_ENV: Joi.string().required(), 
+  CLIENT_URL: Joi.string().required(), //TODO: add .uri() validation when setting correct client uri
+  POSTGRES_HOST: Joi.string().required(),
+  POSTGRES_PORT: Joi.number().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required()
 });
