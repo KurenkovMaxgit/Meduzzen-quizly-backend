ae9523d9557a6a324eb441332de522d9d8d8487b
Maksym Kurenkov <kurenkov.maxim3000@gmail.com>
2026-02-02 15:28:58 +0200

    refactor: separate datasource from app module in dedicated file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { envValidationSchema } from './config/env.schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AppDataSource } from './utils/typeorm.datasource';
 
 @Module({
   imports: [
@@ -15,19 +16,8 @@ import { UserModule } from './user/user.module';
       validationSchema: envValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('dbHost'),
-        port: configService.get<number>('dbPort'),
-        username: configService.get<string>('dbUser'),
-        password: configService.get<string>('dbPassword'),
-        database: configService.get<string>('dbName'),
-        entities: ['common/entities/*.ts'],
-        synchronize: configService.get<string>('nodeEnv') !== 'production',
-        migrations: [__dirname + '../migrations/**/*{.js,.ts}'],
-        migrationsTableName: 'migrations_table',
+      useFactory: () => ({
+        ...AppDataSource.options,
       }),
     }),
     UserModule,
