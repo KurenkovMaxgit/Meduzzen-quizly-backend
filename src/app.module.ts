169f46745eac77bcf9e8fed8ac65c49554eccab5
Maksym Kurenkov <kurenkov.maxim3000@gmail.com>
2026-02-08 05:31:03 +0200

    refactor: update TypeORM datasorce options import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { envValidationSchema } from './config/env.schema';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { join } from 'path';
+import { AppDataSource } from './utils/typeorm.datasource';
 
 @Module({
   imports: [
@@ -16,25 +16,9 @@ import { join } from 'path';
       validationSchema: envValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const isDev = configService.get<string>('nodeEnv') !== 'production';
-        return {
-          type: 'postgres',
-          host: configService.get<string>('dbHost'),
-          port: configService.get<number>('dbPort'),
-          username: configService.get<string>('dbUser'),
-          password: configService.get<string>('dbPassword'),
-          database: configService.get<string>('dbName'),
-          entities: [join(__dirname, '/**/*.entity{.ts,.js}')],
-          migrations: [join(__dirname, '..', 'migrations', '*.ts')],
-          migrationsTableName: 'migrations_table',
-          autoLoadEntities: true, 
-          synchronize: isDev,
-          logging: isDev ? ['info', 'error'] : ['error'],
-        };
-      },
+      useFactory: () => ({
+        ...AppDataSource.options,
+      }),
     }),
     UserModule,
   ],
