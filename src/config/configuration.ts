779b6b59cf952d890e44a4b92f00ee704f655336
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2026-02-12 22:42:45 +0200

    feat: add auth configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,11 +2,16 @@ const configuration = () => ({
   port: Number.parseInt(process.env.PORT ?? '8080', 10) || 8080,
   nodeEnv: process.env.NODE_ENV,
   client: process.env.CLIENT_URL,
-  dbHost: process.env.POSTGRES_HOST,
-  dbPort: Number.parseInt(process.env.POSTGRES_PORT ?? '6540', 10),
-  dbUser: process.env.POSTGRES_USER,
-  dbPassword: process.env.POSTGRES_PASSWORD,
-  dbName: process.env.POSTGRES_DB,
+  jwt: {
+    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
+    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    refreshTokenExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
+  },
+  auth0: {
+    issuerUrl: process.env.AUTH0_ISSUER_URL,
+    audience: process.env.AUTH0_AUDIENCE,
+  },
 });
 
 export type AppConfiguration = ReturnType<typeof configuration>;
