be7448aa623ebf0d95c98b5634a9535c151496f0
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2026-02-12 22:39:29 +0200

    feat: add auth0 strategy & universal auth guard

---
+++ src/auth/strategies/auth0.strategy.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { AppConfiguration } from '../../config/configuration';
+import { passportJwtSecret } from 'jwks-rsa';
+import { AuthService } from '../auth.service';
+import { User } from '../../common/entities/user.entity';
+
+@Injectable()
+export class Auth0Strategy extends PassportStrategy(Strategy, 'auth0') {
+  constructor(
+    private readonly configService: ConfigService<AppConfiguration>,
+    private readonly authService: AuthService,
+  ) {
+    const auth0Config = configService.get('auth0', { infer: true })!;
+    const options: StrategyOptions = {
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${auth0Config.issuerUrl!}.well-known/jwks.json`,
+      }),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      audience: auth0Config.audience,
+      issuer: auth0Config.issuerUrl,
+    };
+    super(options);
+  }
+
+  validate(payload: { 'https://quizly.com/email': string }): Promise<User> {
+    return this.authService.validateOrCreateUserByEmail(payload['https://quizly.com/email']);
+  }
+}
