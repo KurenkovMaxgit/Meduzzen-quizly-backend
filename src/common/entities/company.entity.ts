e97147806360e2aacf7f80c2cf9979b820d85814
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2026-02-17 02:30:44 +0200

    feat: create company entity

---
+++ src/common/entities/company.entity.ts
@@ -0,0 +1,19 @@
+import { Entity, Column, OneToMany } from 'typeorm';
+import { CompanyStatus } from '../../utils/enums';
+import { BaseEntity } from './base.entity';
+import { CompanyUser } from './company-user.entity';
+
+@Entity()
+export class Company extends BaseEntity {
+  @Column('varchar', { length: 250 })
+  name!: string;
+
+  @Column('varchar', { length: 3000 })
+  description!: string;
+
+  @Column({ type: 'enum', enum: CompanyStatus, default: CompanyStatus.VISIBLE })
+  status!: CompanyStatus;
+
+  @OneToMany(() => CompanyUser, (member) => member.company)
+  members?: CompanyUser[];
+}
--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,6 +1,15 @@
 export enum UserRole {
-    OWNER = "owner",
-    ADMIN = "admin",
-    CANDIDATE = "candidate",
-    MEMBER = "member"
+  USER = 'user',
+  SUPER_ADMIN = 'super_admin',
+}
+
+export enum CompanyStatus {
+  VISIBLE = 'visible',
+  HIDDEN = 'hidden',
+}
+
+export enum CompanyRole {
+  OWNER = 'owner',
+  ADMIN = 'admin',
+  MEMBER = 'member',
 }
