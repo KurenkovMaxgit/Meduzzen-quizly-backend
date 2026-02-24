efbd399b37daa0f188137f3b792a7c129cb61309
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2026-02-24 21:14:32 +0200

    feat: add attempt service & controller

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -4,10 +4,13 @@ import { QuizService } from './quiz.service';
 import { Quiz } from '../common/entities/quiz.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from '../company/company.module';
+import { AttemptService } from './attempt.service';
+import { QuizAttempt } from '../common/entities/attempt.entity';
+import { AttemptController } from './attempt.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Quiz]), CompanyModule],
-  controllers: [QuizController],
-  providers: [QuizService, Logger],
+  imports: [TypeOrmModule.forFeature([Quiz, QuizAttempt]), CompanyModule],
+  controllers: [QuizController, AttemptController],
+  providers: [QuizService, AttemptService, Logger],
 })
 export class QuizModule {}
